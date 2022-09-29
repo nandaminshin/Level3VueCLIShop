d5b2e355eb69a5828d905dcc84cdca6579f2d8f0
nandaminshin <nandaminshin2019@gmail.com>
2022-09-29 21:19:28 +0630

    First commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,17 @@
 <template>
+  <Header title="About Page" subtitle="Say Something" />
   <div class="about">
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+  import Header from "@/components/Header.vue";
+
+
+  export default {
+    name: "AboutPart",
+    components: { Header }
+}
+
+</script>
